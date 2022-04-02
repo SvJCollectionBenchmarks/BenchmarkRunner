51fde04f632d9a4181502e17579e542e6f92ff02
Wojciech Kania <wojciech.kania@zoho.com>
2022-04-02 20:32:15 +0200

    Added gc profiler to the command

--- src/main/java/com/adalbert/Main.kt
+++ src/main/java/com/adalbert/Main.kt
@@ -3,7 +3,7 @@ package com.adalbert
 import java.nio.file.Files
 import java.nio.file.Paths
 
-private val disableOverrideCheck = true
+private const val disableOverrideCheck = true
 
 private val projectName = { language: String -> "jmh-$language" }
 private val alphabet = ('a' .. 'z').union('A' .. 'Z').union('0' .. '9')
@@ -12,12 +12,12 @@ private val compileCommand = listOf("C:\\Program Files\\Maven\\bin\\mvn.cmd", "c
 private val listBenchmarksCommand = listOf("java", "-jar", "target\\benchmarks.jar", "-l")
 private val supportedLanguages = listOf("java", "scala")
 
-private val baseCodePath = Paths.get("C:\\Users\\wojci\\source\\master-thesis\\generated\\multiOperationalOwn\\Run_2022-03-18_11-30-46")
+private val baseCodePath = Paths.get("C:\\Users\\wojci\\source\\master-thesis\\generated\\multiOperationalPolya\\Run_2022-04-02_20-24-45\\")
 private val baseOutcomesPath = Paths.get("C:\\Users\\wojci\\source\\master-thesis\\measurements\\raw")
 
 fun main () {
     if (!disableOverrideCheck) {
-        val sequence = (0 until 10).map { alphabet.random() }.joinToString("")
+        val sequence = (0 until 5).map { alphabet.random() }.joinToString("")
         println("If you are sure about the override, enter this sequence: '$sequence'")
         print("Input: ")
         val consoleInput = readLine()
@@ -25,6 +25,7 @@ fun main () {
             println("Operation cancelled!").apply { return }
         else println("Override accepted!")
     }
+
     Files.list(outputsPath).forEach {
         val wasDeleted = it.toFile().deleteRecursively()
         if (!wasDeleted) throw IllegalStateException("Couldn't delete $it")
@@ -37,15 +38,16 @@ fun main () {
         val compileStream = processBuilder.command(compileCommand).startWaitAndRedirect(10)
 //        print(String(compileStream.readNBytes(compileStream.available())))
         val listingStream = processBuilder.command(listBenchmarksCommand).startWaitAndRedirect(3)
-        val benchmarks = String(listingStream.readNBytes(listingStream.available()))
+        val benchmarksRaw = String(listingStream.readNBytes(listingStream.available()))
+        val benchmarks = benchmarksRaw
             .substringAfterLast("Benchmarks:").trim().lines()
             .map { it.substringBeforeLast('.').substringAfterLast('.') }
-            .map { it.substringBeforeLast('_').substringAfterLast('_') }
+            .map { it.substringAfter('_').substringBefore('_') }
             .toSet()
         benchmarks.forEach {
             val absoluteProjectPath = projectDir.absolutePath.replace("\\", "/")
             val absoluteOutcomePath = baseOutcomesPath.add("$it.txt").toString().replace("\\", "/")
-            println("java -jar $absoluteProjectPath/target/benchmarks.jar $it >> $absoluteOutcomePath 2>&1")
+            println("java -jar $absoluteProjectPath/target/benchmarks.jar $it -prof gc >> $absoluteOutcomePath 2>&1")
         }
     }
 
