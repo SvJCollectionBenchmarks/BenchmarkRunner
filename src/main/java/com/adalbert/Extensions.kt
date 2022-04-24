93a6efe189cb225347d98dc31f694f1022169741
Wojciech Kania <wojciech.kania@zoho.com>
2022-04-24 10:47:10 +0200

    Divided results into directories

--- src/main/java/com/adalbert/Extensions.kt
+++ src/main/java/com/adalbert/Extensions.kt
@@ -1,6 +1,5 @@
 package com.adalbert
 
-import java.io.File
 import java.io.InputStream
 import java.nio.file.Path
 import java.nio.file.Paths
--- src/main/java/com/adalbert/Main.kt
+++ src/main/java/com/adalbert/Main.kt
@@ -1,42 +1,53 @@
 package com.adalbert
 
 import java.nio.file.Files
+import java.nio.file.Path
 import java.nio.file.Paths
 
 private const val disableOverrideCheck = false
 
 private val projectName = { language: String -> "jmh-$language" }
 private val alphabet = ('a' .. 'z').union('A' .. 'Z').union('0' .. '9')
-private val outputsPath = Paths.get("C:\\Users\\wojci\\source\\master-thesis\\measurements\\raw")
 private val compileCommand = listOf("C:\\Program Files\\Maven\\bin\\mvn.cmd", "clean", "install")
 private val listBenchmarksCommand = listOf("java", "-jar", "target\\benchmarks.jar", "-l")
 private val supportedLanguages = listOf("java", "scala")
 
-private val baseCodePath = Paths.get("C:\\Users\\wojci\\source\\master-thesis\\generated\\multiOperationalPolya\\Run_2022-04-10_17-51-33\\")
+private val baseCodePath = Paths.get("C:\\Users\\wojci\\source\\master-thesis\\generated\\singleOperational\\Run_2022-04-22_20-09-50\\")
 private val baseOutcomesPath = Paths.get("C:\\Users\\wojci\\source\\master-thesis\\measurements\\raw")
 
+fun deleteFilesNotDirectories(path: Path) {
+    Files.list(path).forEach {
+        val file = it.toFile()
+        if (file.isDirectory)
+            deleteFilesNotDirectories(it)
+        file.delete() || throw IllegalStateException("Couldn't delete $it!")
+    }
+}
+
 fun main () {
+    val baseCodeString = baseCodePath.toString()
+    val finalOutputsPath = if (baseCodeString.contains("polya", true)) baseOutcomesPath.add("polya")
+        else if (baseCodeString.contains("single", true)) baseOutcomesPath.add("single")
+        else baseOutcomesPath.add("own")
+
     if (!disableOverrideCheck) {
         val sequence = (0 until 5).map { alphabet.random() }.joinToString("")
+        println("About to override the $finalOutputsPath directory!")
         println("If you are sure about the override, enter this sequence: '$sequence'")
         print("Input: ")
         val consoleInput = readLine()
         if (consoleInput != sequence)
             println("Operation cancelled!").apply { return }
-        else println("Override accepted!")
-    }
-
-    Files.list(outputsPath).forEach {
-        val wasDeleted = it.toFile().deleteRecursively()
-        if (!wasDeleted) throw IllegalStateException("Couldn't delete $it")
+        println("Override accepted!")
+        deleteFilesNotDirectories(finalOutputsPath)
     }
 
     supportedLanguages.forEach { language ->
         val projectDir = baseCodePath.add(projectName(language)).toFile()
         val processBuilder = ProcessBuilder().directory(projectDir)
-        val compileStream = processBuilder.command(compileCommand).startWaitAndRedirect(40)
+        val compileStream = processBuilder.command(compileCommand).startWaitAndRedirect(90)
 //        print(String(compileStream.readNBytes(compileStream.available())))
-        val listingStream = processBuilder.command(listBenchmarksCommand).startWaitAndRedirect(5)
+        val listingStream = processBuilder.command(listBenchmarksCommand).startWaitAndRedirect(10)
         val benchmarksRaw = String(listingStream.readAllBytes())
         val benchmarks = benchmarksRaw
             .substringAfterLast("Benchmarks:").trim().lines()
@@ -45,8 +56,8 @@ fun main () {
             .toSet()
         benchmarks.forEach {
             val absoluteProjectPath = projectDir.absolutePath.replace("\\", "/")
-            val absoluteOutcomePath = baseOutcomesPath.add("$it.txt").toString().replace("\\", "/")
-            println("java -jar $absoluteProjectPath/target/benchmarks.jar $it -prof gc >> $absoluteOutcomePath 2>&1")
+            val absoluteOutcomePath = finalOutputsPath.add("$it.txt").toString().replace("\\", "/")
+            println("java -jar $absoluteProjectPath/target/benchmarks.jar ${it}_ -prof gc >> $absoluteOutcomePath 2>&1")
         }
     }
 
